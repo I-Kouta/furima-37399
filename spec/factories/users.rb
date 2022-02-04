b417ef4338b3a33b4a13e1347216b6b18afcdb02
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-04 13:09:10 +0900

    fakerの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name             {"岩淵"}
+    first_name            {"昂太"}
+    last_name_readinig    {"イワブチ"}
+    first_name_reading    {"コウタ"}
+    barthday_id           {"1999-02-22"}
   end
 end
