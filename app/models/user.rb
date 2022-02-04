aa600a97306623d9ca1d8d8e5d23a9d3a3a595a9
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-04 18:35:02 +0900

    エラーメッセージの重複の解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,           presence: true
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :last_name_reading,  presence: true
-  validates :first_name_reading, presence: true
   validates :barthday_id,        presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
