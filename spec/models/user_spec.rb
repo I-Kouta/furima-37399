a4b09662005f77787e36c9df7b510649d9111dca
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-04 12:54:39 +0900

    モデル単体テストコードの雛形の作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,18 +6,30 @@ RSpec.describe User, type: :model do
 
   describe "ユーザー新規登録" do
     context "新規登録できる" do
-      it "必須項目が全て記入されていれば登録ができる"
+      it "必須項目が全て記入されていれば登録ができる" do
+      end
+      it "パスワードが半角英数字混合で6文字以上なら登録ができる" do
+      end
+    end
+    context "新規登録できない" do
+      it "ニックネームが入力されていない" do
+      end
+      it "重複したemailが存在している" do
+      end
+      it "emailが@を含んでいない" do
+      end
+      it "パスワードが入力されていない" do
+      end
+      it "パスワードが5文字以下である" do
+      end
+      it "パスワードが半角英数字混合でない" do
+      end
+      it "パスワードとパスワード(確認)が一致しない" do
+      end
+      it "名前欄が入力されていない" do
+      end
+      it "生年月日が入力されていない" do
+      end
+    end
   end
-
-
-
-
-
-
-
-
-
-
-
-
 end
