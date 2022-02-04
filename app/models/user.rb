aed41eb4430e81a513e7cee7f3d75b3dcf02db65
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-04 18:02:51 +0900

    userモデルのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,13 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
-  has_many :items
-  has_many :records
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+    validates :last_name_reading
+    validates :first_name_reading
+  end
+
+  # has_many :items
+  # has_many :records
 end
