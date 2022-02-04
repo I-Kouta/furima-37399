14ef2fe5f16b1a6293d44371c41f562d9fb7d3aa
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-04 12:30:58 +0900

    fakerの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+  end
+
+  describe "ユーザー新規登録" do
+    context "新規登録できる" do
+      it "必須項目が全て記入されていれば登録ができる"
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
