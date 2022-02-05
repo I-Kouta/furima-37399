41ac6e5e56fc0826d1e2774465cf2dc649072702
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-05 18:34:26 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  # has_one    :record
+  belongs_to       :user
+  #has_one          :record
+  has_one_attached :image
+
+  validates :title,  presence: true
+  validates :content,  presence: true
+  validates :genre_id,  presence: true
+  validates :condition_id,  presence: true
+  validates :cost_id,  presence: true
+  validates :prefecture_id,  presence: true
+  validates :date_id,  presence: true
+  validates :price,  presence: true
 end
