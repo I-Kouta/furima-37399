9f84fe84dc407d8bfc641c597f9f87248c915bcb
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-06 13:59:38 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
-  validates :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :date_id, :price, :image, presence: true
+  validates :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, :price, :image, presence: true
 
   validates :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
 end
