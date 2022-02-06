97aecc876e1312d112c698e5257552f513097ffc
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-06 12:57:51 +0900

    カテゴリー選択のアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,11 @@ class Item < ApplicationRecord
   belongs_to       :user
   # has_one          :record
   has_one_attached :image
+  belongs_to :genre
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
 
   validates :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :date_id, :price, :image, presence: true
 
