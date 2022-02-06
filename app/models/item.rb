5757418d9a1423df8c9dde78470685a95223c0bd
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-06 12:45:49 +0900

    画像のバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   # has_one          :record
   has_one_attached :image
 
-  validates :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :date_id, :price, presence: true
+  validates :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :date_id, :price, :image, presence: true
 
   validates :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
 end
