ef172b0c9b41467a38e2ae99260641767a6fe328
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-06 13:16:30 +0900

    商品出品画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,15 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
