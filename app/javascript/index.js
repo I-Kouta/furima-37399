d99eb5bac62fe259b5fe0540ac849093d64a27e5
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-07 12:31:15 +0900

    販売手数料と利益を表示させる準備

---
+++ app/javascript/index.js
@@ -0,0 +1,7 @@
+function price(){
+  const itemPrice = document.getElementById("item-price");
+  const addTax = document.getElementById("add-tax-price");
+  const Profit = document.getElementById("profit");
+}
+
+window.addEventListener('load', price);
