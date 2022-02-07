94d375260f785d0a2609fdea107846adf9af84e7
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-07 16:11:30 +0900

    価格の表示・最大値・最小値の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,11 @@ class Item < ApplicationRecord
   belongs_to :shipping_date
 
   validates :image, :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, :price, presence: true
+  
+  validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters'}
 
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  
   validates :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
