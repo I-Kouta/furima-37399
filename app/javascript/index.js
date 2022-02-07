7ff6b506c7216a60ba67e6f13ff1e659a8ef6a36
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-07 14:36:28 +0900

    販売手数料と販売利益を整数で表示させる記述

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,15 +1,12 @@
 function price(){
   const itemPrice = document.getElementById("item-price");
   
-  
   itemPrice.addEventListener("keyup", () => {
-    const countTax = itemPrice.value * 0.1;
+    const countTax = Math.round(itemPrice.value * 0.1);
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${countTax}`
-  });
-  
-  itemPrice.addEventListener("keyup", () => {
-    const countProfit = itemPrice.value * 0.9;
+    
+    const countProfit = Math.round(itemPrice.value * 0.9);
     const Profit = document.getElementById("profit");
     Profit.innerHTML = `${countProfit}`
   });
