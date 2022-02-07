1e3b3b570bbe6b8db2db34e7da1bf0b8111dc5ce
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-07 16:55:23 +0900

    テスト用の画像の用意

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,16 @@ FactoryBot.define do
   factory :item do
     title              {Faker::Lorem.sentence}
     content            {Faker::Lorem.sentence}
-    genre_id           {}
-    condition_id       {}
-    cost_id            {}
-    prefecture_id      {}
-    shipping_date_id   {}
-    price              {}
-    user
+    genre_id           {2}
+    condition_id       {2}
+    cost_id            {2}
+    prefecture_id      {2}
+    shipping_date_id   {2}
+    price              {3000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
