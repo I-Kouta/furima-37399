dd2206c3c364af3b3605e25d0ee1fde026195259
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-07 10:55:34 +0900

    画像のバリデーションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
