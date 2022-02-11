f8924e9a6dc0506049bd7791589021fe84c4d21c
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-11 21:30:02 +0900

    tokenのバリデーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   belongs_to :item
   has_one    :address
   attr_accessor :token
+  validates :token, presence: true
 
   validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   
