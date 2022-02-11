db4f00430d45ba7dc538e8c5438de3a73c135341
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-11 17:41:25 +0900

    保存のための記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,5 +6,18 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
   end
 end
