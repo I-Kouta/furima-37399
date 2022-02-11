1a805e91f495b9ee75fbb27e2f89f4426d65a23e
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-11 17:24:54 +0900

    バリデーションの記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one    :address
+
+  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  
+  validates :city, :city_number, presence: true
+
+  validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
 end
