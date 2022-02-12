f75853aa10152a50dae6147672301d460c2c6aca
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-12 15:38:24 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  
+  validates :city, :city_number, presence: true
+
+  validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,11 +5,4 @@ class Order < ApplicationRecord
   attr_accessor :token
   validates :token, presence: true
 
-  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  
-  validates :city, :city_number, presence: true
-
-  validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
 end
