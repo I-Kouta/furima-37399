b7640fe1def25ae21524c535380fca63fe100bd1
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-14 18:42:59 +0900

    コントローラーの定義と分岐の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,25 +2,26 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @order = Order.new
+    @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
   end
   
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
       pay_item
-      @order.save
+      @order_address.save
       redirect_to root_path
     else
-      render 'index'
+      @item = Item.find(params[:item_id])
+      render :index
     end
   end
   
   private
 
   def order_params
-    params.require(:address).permit(:post_code, :prefecture_id, :city, :city_number, :building, :phone_number)
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :city_number, :building, :phone_number).merge(user_id: current_user.id)
   end
 
   def pay_item
