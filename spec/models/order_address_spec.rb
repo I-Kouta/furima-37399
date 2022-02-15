e07848a5b3a7a87bd2f96562482a36285bc96d8e
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-15 17:26:38 +0900

    正常形テストの実行

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '購入内容の保存' do
     before do
-      @order_address = FactoryBot.build(:order_address)
       user = FactoryBot.create(:user)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id)
     end
     context '購入ができる場合' do
       it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@order_address).to be_valid
       end
-      it 'buildingは空でも保存できること' do
+      it 'buildingは空でも保存できる' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
       end
     end
     
