9f12a5a319f0f8163b0dc2e19efbfb0f522cf2c8
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-15 11:23:33 +0900

    FactoryBotでのデータ生成

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    post_code     { '111-2222' }
+    prefecture_id { 2 }
+    city          { '東京都' }
+    city_number   { '東京都' }
+    building      { '建物名' }
+    phone_number  { '08011112222' }
+    token         {"tok_abcdefghijk00000000000000000"}
+  end
+end
