9ab156cda93e7d74a619026be39edaa09049861d
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-15 17:03:39 +0900

    order_addressモデルのFactoryBotの記述

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     post_code     { '111-2222' }
     prefecture_id { 2 }
     city          { '東京都' }
-    city_number   { '東京都' }
+    city_number   { '渋谷区' }
     building      { '建物名' }
     phone_number  { '08011112222' }
     token         {"tok_abcdefghijk00000000000000000"}
