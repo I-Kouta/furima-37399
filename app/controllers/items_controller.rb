4efe6af3079c403f8a7a1f8f06d4ead315c9a973
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-15 20:22:07 +0900

    出品者以外が編集できないようにするための記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index if @item.order.present? || current_user.id == @item.user_id
+    redirect_to action: :index if @item.order.present? || current_user.id != @item.user_id
   end
 
   def update
