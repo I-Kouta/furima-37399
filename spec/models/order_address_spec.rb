42b2b84808094b3ad933089e8bf9c4bb7f38386d
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-16 15:10:19 +0900

    テストコード実行の際の記述の削除

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe OrderAddress, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
-      sleep(0.1)
     end
     context '購入ができる場合' do
       it 'すべての値が正しく入力されていれば保存できる' do
