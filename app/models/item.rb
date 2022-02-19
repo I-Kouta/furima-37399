9ee5d274699c2028ba97efbe315894013fd218fd
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-19 13:30:44 +0900

    itemモデルのエラーメッセージの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
   validates :image, :title, :content, :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id, :price,
             presence: true
 
-  validates :price, numericality: { only_integer: true, message: ' is invalid. Input half-width characters' }
+  validates :price, numericality: { only_integer: true, message: 'は半角数字で入力してください' }
 
   validates :price,
-            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: ' is out of setting range' }
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'が設定範囲外です' }
 
   validates :genre_id, :condition_id, :cost_id, :prefecture_id, :shipping_date_id,
-            numericality: { other_than: 1, message: " can't be blank" }
+            numericality: { other_than: 1, message: "が選択されていません" }
 end
