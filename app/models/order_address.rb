bf2b7001a9115e98003b2d36ff9478726c6d6dcd
I-Kouta <kouta.baseball0222@gmail.com>
2022-02-19 13:42:29 +0900

    購入画面のエラーメッセージの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,11 +4,11 @@ class OrderAddress
 
   with_options presence: true do
     validates :token, :city, :city_number, :phone_number, :user_id, :item_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: ' is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: ' は以下の様に入力してください (e.g. 123-4567)' }
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: " can't be blank" }
-  validates :phone_number, numericality: { only_integer: true, message: ' is invalid. Input only number' }
-  validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: ' is out of setting range' }
+  validates :prefecture_id, numericality: { other_than: 1, message: "が選択されていません" }
+  validates :phone_number, numericality: { only_integer: true, message: 'は半角で入力してください' }
+  validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'が有効ではありません' }
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
